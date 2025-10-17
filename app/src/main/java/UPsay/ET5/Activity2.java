6535e0b30e5c253321ab342058e1a5c65aa4e39d
afauver <fauvernier.alexandre@gmail.com>
2025-10-17 11:36:29 +0200

    tp

---
+++ app/src/main/java/UPsay/ET5/Activity2.java
@@ -0,0 +1,40 @@
+package UPsay.ET5;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity2 extends AppCompatActivity {
+    public static final String EXTRA = "message";
+    EditText monTexte;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void retour(View view) {
+        Intent messageIntent = new Intent();
+        monTexte = findViewById(R.id.MonTexte);
+        String message = monTexte.getText().toString();
+        messageIntent.putExtra(EXTRA, message);
+        setResult(Activity.RESULT_OK, messageIntent);
+        finish();
+    }
+}
--- app/src/main/java/UPsay/ET5/MainActivity.java
+++ app/src/main/java/UPsay/ET5/MainActivity.java
@@ -1,24 +1,52 @@
 package UPsay.ET5;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityResultLauncher<Intent>
+            lanceActivity2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        lanceActivity2 = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            String message =
+                                    data.getStringExtra(Activity2.EXTRA);
+                            TextView textView =
+                                    findViewById(R.id.textView);
+                            textView.setText(message);
+                        }
+                    }
+                });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void lanceActivity2(View view) {
+        Intent intent = new Intent(this, Activity2.class);
+        lanceActivity2.launch(intent);
     }
 }
